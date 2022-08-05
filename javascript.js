f9aae9394bc95841bf63047875c4aa49ade6314b
VanDanh <tranvandanh157@gmail.com>
2022-08-05 16:37:31 +0700

    update demo

---
+++ javascript.js
@@ -0,0 +1,36 @@
+$(".owl-service").owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: false,
+  dots: false,
+
+  responsive: {
+    0: {
+      items: 2,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 4,
+    },
+  },
+});
+$(".owl-companion").owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: false,
+  dots: false,
+
+  responsive: {
+    0: {
+      items: 2,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
